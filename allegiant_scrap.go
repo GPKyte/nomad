fc920992f98b97a1de264f30d1f740ad1bde06cb
Gavin Kyte <gpkyte@gmail.com>
2020-03-04 03:10:02 -0500

    Reduce and Iterate towards scraping of deals page

--- allegiant_scrap.go
+++ allegiant_scrap.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gocolly/colly"
 )
 
+// BasicHTMLError is a simple std for error reporting
 type BasicHTMLError struct {
 	context string
 	want    string
@@ -17,11 +18,7 @@ type BasicHTMLError struct {
 }
 
 func rapidTest(c *colly.Collector) error {
-	c.Visit("https://flight.deals.allegiant.com/ats/url.aspx?cr=986&wu=95&camp=20200223_SundaySavings&allow_search=1")
-	err := c.OnHTML("div.flight-deal-card", func(e *colly.HTMLElement) {
-		fmt.Print(e.ChildTexts("div.origin"))
-	})
-	return err
+	return nil
 }
 
 func (e *BasicHTMLError) Error() string {
@@ -34,78 +31,54 @@ func (e *BasicHTMLError) Error() string {
 	return fmt.Sprintf("Probably Parsing related;\nWant: %s\nGot: %s\n%s", e.want, e.got, contextLine)
 }
 
-func makeDefaultCollector() colly.Collector {
+func makeDefaultCollector() *colly.Collector {
 	return colly.NewCollector(
 		colly.AllowedDomains("allegiant.com"),
 		colly.MaxDepth(2),
 		colly.UserAgent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:73.0), Gecko/20100101 Firefox/73.0"),
-		colly.ParseHTTPErrorResponse(global.DEBUG), /* TODO: make this global var work */
-		colly.MaxBodySize(maxint),
-		colly.CacheDir(""),
-		colly.Async(false),
+		colly.CacheDir("resources/cache/allegiant"),
+		colly.Async(false), /* Maybe True... Allegiant uses AJAX for normal searches */
 	)
 }
 
-type LPAllegiant ListingParser
-
-// RecordScrapestamp returns a default timeSpace because this is the default Listing class
-func (LP *ListingParser) RecordScrapestamp(L *Listing) err {
-	location := LP.url
-	datetime := Now()
-	L.ScrapeStamp = timeSpace{Location: location, DateTime: datetime}
-	return nil
-}
-
-// RecordDeparture returns a default timeSpace because this is the default Listing class
-func (LP *ListingParser) RecordDeparture(L *Listing) err {
-
-	return nil
-}
-
-// RecordArrival returns a default timeSpace because this is the default Listing class
-func (LP *ListingParser) RecordArrival(L *Listing) err {
-	L.Arrive = timeSpace{}
-	return nil
-}
-
-// RecordPrice returns a default timeSpace because this is the default Listing class
-func (LP *ListingParser) RecordPrice(L *Listing) err {
-	L.Price = 0
-	return nil
-}
-
-type flightDealCard struct {
+type allegiantDealListing struct {
 	departLocation     string
 	arriveLocation     string
 	effectiveDateRange string
-	price              int
+	price              string
 	duration           string
 }
 
-func (card *flightDealCard) String() string {
+func (card *allegiantDealListing) String() string {
 	return fmt.Sprintf("From %v\nTo %v\nFor $%v", card.departLocation, card.arriveLocation, card.price)
 }
 
-func (c *colly.Collector) scrapeDealsPage(link string) ([]flightDealCard, error) {
+func isNil(value string) bool {
+	return value == ""
+}
+
+func scrapeDealsPage(link string) ([]allegiantDealListing, error) {
+	c := makeDefaultCollector()
 	c.Visit(link)
 
 	Wait(504)
-	var flightDeals []flightDealCard
+	var flightDeals []allegiantDealListing
 	c.OnHTML("div.flight-deal-card", func(flightDealCard *colly.HTMLElement) {
-		departLocation := flightDealCard.ChildAttr("div.origin")
-		arriveLocation := flightDealCard.ChildAttr("div.destination")
-		effectiveDateRange := flightDealCard.ChildAttr("div.line3")
-		price := flightDealCard.ChildAttr("div.pc-price")
-		duration := flightDealCard.ChildAttr("span.flight-time span")
+		departLocation := flightDealCard.ChildText("div.origin")
+		arriveLocation := flightDealCard.ChildText("div.destination")
+		effectiveDateRange := flightDealCard.ChildText("div.line3")
+		price := flightDealCard.ChildText("div.pc-price")
+		duration := flightDealCard.ChildText("span.flight-time span")
 
-		if price != nil {
-			append(flightDeals, fmt.Println(flightDealCard{
+		if isNil(price) {
+			newCard := allegiantDealListing{
 				departLocation:     departLocation,
 				arriveLocation:     arriveLocation,
 				effectiveDateRange: effectiveDateRange,
 				price:              price,
 				duration:           duration,
-			}))
+			}
+			flightDeals = append(flightDeals, newCard)
 		}
 	})
 
@@ -114,9 +87,7 @@ func (c *colly.Collector) scrapeDealsPage(link string) ([]flightDealCard, error)
 
 func main() {
 	// Instantiate default collector
-	c = makeCollector()
-
 	justALinkForNow := "https://flight.deals.allegiant.com/ats/url.aspx?cr=986&wu=95&camp=20200301_SundaySavings&allow_search=1"
 	flightDeals, err := scrapeDealsPage(justALinkForNow)
-	fmt.Println(flightDeals...)
+	fmt.Println(flightDeals)
 }
--- allegiant_scrap_test.go
+++ allegiant_scrap_test.go
@@ -5,35 +5,10 @@ import (
 	"testing"
 )
 
-var HTMLAllegiantListingContext string = readFile("resources/Allegiant1.html")
+// var HTMLAllegiantListingContext string = readFile("resources/Allegiant1.html")
 
 func TestRapidTest(t *testing.T) {
 	justALinkForNow := "https://flight.deals.allegiant.com/ats/url.aspx?cr=986&wu=95&camp=20200301_SundaySavings&allow_search=1"
 	flightDeals, err := scrapeDealsPage(justALinkForNow)
-	fmt.Println(flightDeals...)
-}
-
-func TestGetAllDailyDealListings(t *testing.T) {
-
-}
-
-func TestGetADailyDealListing(t *testing.T) {
-	siteContent := HTMLAllegiantListingContext
-	var LP LPAllegiant
-	LP.URL = ""
-
-	L * Listing, err = LP.pointToListingFromThis(siteContent)
-	if err != nil {
-		t.Fail(err)
-	}
-}
-
-func TestStringMethod(t *testing.T) {
-	L := Listing{}
-	goal := ""
-	got := string(L)
-}
-
-func TestComparison(t *testing.T) {
-	t.Fail
+	fmt.Println(flightDeals)
 }
