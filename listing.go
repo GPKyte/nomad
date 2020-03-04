53738fafebde0b3fe0f1be044455cac92ffcf06a
Gavin Kyte <gpkyte@gmail.com>
2020-03-04 01:52:46 -0500

    Remove poorly modeled Parsing interface

--- listing.go
+++ listing.go
@@ -1,6 +1,9 @@
 package parse
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 /*
 Listing contains the necessary data to describe an online travel listing
@@ -15,17 +18,8 @@ type Listing struct {
 }
 
 type timeSpace struct {
-	Date      string
-	TimeOfDay int    /* Value between {0 ... (24 * 60)} */
-	Location  string /* standardized to lowercase, UTF8 default */
-}
-
-// ListingParser will form the template for site-specific parsers
-type ListingParser interface {
-	RecordScrapestamp() timeSpace
-	RecordDeparture() timeSpace
-	RecordArrival() timeSpace
-	RecordPrice() int
+	DateTime time.Time
+	Location string /* standardized to lowercase, UTF8 default */
 }
 
 type readable interface {
@@ -35,19 +29,6 @@ type readable interface {
 
 func newListing() Listing {
 	return Listing{}
-}
-
-// Listing is an initializer that takes context-dependent data and scrapes it
-func (LP *ListingParser) pointToListingFromThis(parsableSrcContent string) *Listing {
-	var L Listing = newListing()
-
-	L.srcContent = parsableSrcContent
-	LP.RecordScrapestamp(L)
-	LP.RecordDeparture(L)
-	LP.RecordArrival(L)
-	LP.RecordPrice(L)
-
-	return &L
 }
 
 type ValidDataObj interface {
@@ -81,31 +62,14 @@ func (L *Listing) isValid() bool {
 
 /* TODO: unexport default methods once testing confirms okay. fmt implies timeSpace should be exported if Record* is */
 
-// RecordScrapestamp returns a default timeSpace because this is the default Listing class
-func (LP *ListingParser) RecordScrapestamp(L *Listing) err {
-	L.ScrapeStamp = timeSpace{}
-	return nil
-}
-
-// RecordDeparture returns a default timeSpace because this is the default Listing class
-func (LP *ListingParser) RecordDeparture(L *Listing) err {
-	L.Depart = timeSpace{}
-	return nil
-}
-
-// RecordArrival returns a default timeSpace because this is the default Listing class
-func (LP *ListingParser) RecordArrival(L *Listing) err {
-	L.Arrive = timeSpace{}
-	return nil
-}
-
-// RecordPrice returns a default timeSpace because this is the default Listing class
-func (LP *ListingParser) RecordPrice(L *Listing) err {
-	L.Price = 0
-	return nil
+func recordCurrentTimeSpace(location string) timeSpace {
+	return timeSpace{
+		DateTime: time.Now().UTF8(),
+		Location: location,
+	}
 }
 
 // String will return JSON Repr of Listing Or Flat Repr
-func (nestedStruct Listing) String() string {
-	return fmt.Sprint("TODO: Show pretty print of Listing")
+func (L *Listing) String() string {
+	return fmt.Sprintf("From: %s\nTo: %s\nCost: %v\nStamp: %s\n.............................", L.Depart, L.Arrive, L.Price, L.Scrape)
 }
