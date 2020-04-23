94c467d79426adfb0ef708c0e1ce0410964abfc0
Gavin Kyte <gpkyte@gmail.com>
2020-04-23 15:17:11 -0400

    Load location data from cache
    Wrote a util to load locations a while back
    saving it to move on and overright

--- allegiant.go
+++ allegiant.go
@@ -2,6 +2,7 @@ package nomad
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"regexp"
 	"strings"
@@ -151,4 +152,62 @@ func cache(refname string, data []byte) {
 func parseFromCache(path string) []Listing {
 	// TODO: read n files from cache location, should be agnostic of naming convention
 	return make([]Listing, 0)
+}
+
+func loadAllegiantLocationData(index int) []location {
+	var allegiantLocationData []byte = ioutil.ReadFile("resources/allegiant/locations.txt")
+	var locations []location
+
+	newlineMarkers := make(chan int)
+	lines := make(chan string)
+	done := make(chan bool)
+
+	// Parse each location into data struct on init
+	go func() {
+		locations = make([]string, 100)
+
+		for {
+			locData, more := <-lines
+
+			if more {
+				commaIndex := strings.IndexByte(locData, byte(','))
+				afterParenIndex := strings.IndexByte(locData, byte('(')) + 1 // Don't want paren in the code
+
+				name := locData[:commaIndex]
+				state := locData[commaIndex+len(", ") : commaIndex+len(", XY")] // Get 'XY'
+				code := locData[afterParenIndex : afterParenIndex+len("ABC")]   // Get 'ABC'
+
+				locations = append(locations, location{Name: name, State: state, Code: code})
+			} else {
+				done <- true
+				return
+			}
+		}
+	}()
+
+	go func() {
+		start := 0
+		for {
+			nextDelim, more := <-newlineMarkers
+
+			if more {
+				lines <- string(allegiantLocationData[start:nextDelim])
+				start = nextDelim + 1
+
+			} else {
+				lines <- string(allegiantLocationData[start:])
+				return
+			}
+		}
+	}()
+
+	// Find all newline characters deliminating lines
+	for i := 0; i < len(allegiantLocationData); i++ {
+		if allegiantLocationData[i] == '\n' {
+			newlineMarkers <- i
+		}
+	}
+
+	<-done
+	return locations
 }
