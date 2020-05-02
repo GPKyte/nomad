4537c1c638840a1b1a33aa58dece9893f3d45ea5
Gavin Kyte <gpkyte@gmail.com>
2020-05-02 14:55:30 -0400

    Fuck strong typing

--- common/queue.go
+++ common/queue.go
@@ -7,34 +7,34 @@ type queue interface {
 	deQ()
 }
 
-type node interface {
+type qnode interface {
 	value() string
 	next()
 }
 
 /* Will break with high volume */
 type basicQ struct {
-	container []node
+	container []qnode
 	head      int
 	tail      int
 }
 
 func newBasicQueue() *basicQ {
 	var Q = new(basicQ)
-	Q.container = make([]node, 1<<10)
-	Q.head = 0 /* If head < tail, there exist enQ'd nodes */
+	Q.container = make([]qnode, 1<<10)
+	Q.head = 0 /* If head < tail, there exist enQ'd qnodes */
 	Q.tail = 0
 	return Q
 }
 
-func (Q *basicQ) enQ(n *node) {
+func (Q *basicQ) enQ(n *qnode) {
 	Q.tail++
 	index := Q.tail % len(Q.container)
 	Q.container[index] = *n
 }
 
-func (Q *basicQ) deQ() *node {
-	var result *node
+func (Q *basicQ) deQ() *qnode {
+	var result *qnode
 
 	if Q.head >= Q.tail {
 		result = nil
